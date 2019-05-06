4f9c857b57ca69f1c4cc0eb68d72c4ec6b91433f
Dmitry Solntsev <dmityhuyan@gmail.com>
2019-05-06 22:28:33 +0300

    small changes

--- src/ReadImage.cpp
+++ src/ReadImage.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <iostream>
+#include <exception>
+#include <string>
 #include "opencv2/opencv.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
 
 using namespace cv;
 using namespace std;
@@ -9,10 +13,10 @@ int main(int argc, char **argv) {
   Mat image;
   image = imread("../TestData/test_crop.jpg");
   if (!image.data) {
-    cout << "No image data \n";
-    return -1;
+    throw logic_error("No image data!\n");
   }
-  namedWindow("Display Image", WINDOW_GUI_NORMAL);
+  namedWindow("Display Image", WindowFlags::WINDOW_FREERATIO);
+  resizeWindow("Display Image", {1920, 1080});
   imshow("Display Image", image);
   waitKey(0);
   return 0;
