1baa65d162e9933623e5b743d2be47e0616e97d3
Dmitry Solntsev <dmityhuyan@gmail.com>
2019-05-06 22:15:53 +0300

    fixed image filepath

--- src/ReadImage.cpp
+++ src/ReadImage.cpp
@@ -1,26 +1,19 @@
 #include <stdio.h>
-#include "opencv2/opencv.hpp"
 #include <iostream>
+#include "opencv2/opencv.hpp"
 
 using namespace cv;
 using namespace std;
 
-int main(int argc, char **argv)
-{
-    // if (argc != 2)
-    // {
-    //     cout << "usage: DisplayImage.out <Image_Path>\n";
-    //     return -1;
-    // }
-    Mat image;
-    image = imread("/home/dmitry/CppProjects/PiScanner/TestData/test_crop.jpg");
-    if (!image.data)
-    {
-        cout << "No image data \n";
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display Image", image);
-    waitKey(0);
-    return 0;
+int main(int argc, char **argv) {
+  Mat image;
+  image = imread("../TestData/test_crop.jpg");
+  if (!image.data) {
+    cout << "No image data \n";
+    return -1;
+  }
+  namedWindow("Display Image", WINDOW_GUI_NORMAL);
+  imshow("Display Image", image);
+  waitKey(0);
+  return 0;
 }
